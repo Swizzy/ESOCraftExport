e7b18113ec7d58174bbf61e4c5274c87ac7d8095
Swizzy <Swizzy@xeupd.com>
2014-06-05 01:49:54 +0200

    Initial Client Code Commit
    Signed-off-by: Swizzy <Swizzy@xeupd.com>

---
+++ ESOCraftExport/ESOCraftExport/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ESOCraftExport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ServerListBox = new System.Windows.Forms.ComboBox();
+            this.StylesBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.AccountBox = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.CharacterBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ServerListBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 46);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // ServerListBox
+            // 
+            this.ServerListBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ServerListBox.FormattingEnabled = true;
+            this.ServerListBox.Location = new System.Drawing.Point(6, 19);
+            this.ServerListBox.Name = "ServerListBox";
+            this.ServerListBox.Size = new System.Drawing.Size(123, 21);
+            this.ServerListBox.TabIndex = 0;
+            this.ServerListBox.SelectedIndexChanged += new System.EventHandler(this.ServerListBox_SelectedIndexChanged);
+            // 
+            // StylesBox
+            // 
+            this.StylesBox.Location = new System.Drawing.Point(486, 38);
+            this.StylesBox.Name = "StylesBox";
+            this.StylesBox.Size = new System.Drawing.Size(120, 212);
+            this.StylesBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Styles:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.AccountBox);
+            this.groupBox2.Location = new System.Drawing.Point(153, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(135, 46);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Account";
+            // 
+            // AccountBox
+            // 
+            this.AccountBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.AccountBox.FormattingEnabled = true;
+            this.AccountBox.Location = new System.Drawing.Point(6, 19);
+            this.AccountBox.Name = "AccountBox";
+            this.AccountBox.Size = new System.Drawing.Size(123, 21);
+            this.AccountBox.TabIndex = 0;
+            this.AccountBox.SelectedIndexChanged += new System.EventHandler(this.AccountBox_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.CharacterBox);
+            this.groupBox3.Location = new System.Drawing.Point(294, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(186, 46);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Character";
+            // 
+            // CharacterBox
+            // 
+            this.CharacterBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CharacterBox.FormattingEnabled = true;
+            this.CharacterBox.Location = new System.Drawing.Point(6, 19);
+            this.CharacterBox.Name = "CharacterBox";
+            this.CharacterBox.Size = new System.Drawing.Size(174, 21);
+            this.CharacterBox.TabIndex = 0;
+            this.CharacterBox.SelectedIndexChanged += new System.EventHandler(this.CharacterBox_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StylesBox);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox ServerListBox;
+        private System.Windows.Forms.ListBox StylesBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox AccountBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox CharacterBox;
+    }
+}
+
---
+++ ESOCraftExport/ESOCraftExport/MainForm.cs
@@ -0,0 +1,143 @@
+﻿namespace ESOCraftExport {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Windows.Forms;
+
+    public partial class MainForm: Form {
+        public MainForm() {
+            InitializeComponent();
+            foreach(var s in Directory.GetDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Elder Scrolls Online")))
+                ServerListBox.Items.Add(new Server(s));
+            ServerListBox.SelectedIndex = 0;
+        }
+
+        private void ServerListBox_SelectedIndexChanged(object sender, EventArgs e) { AccountBox.DataSource = ((Server)ServerListBox.SelectedItem).Accounts; }
+
+        private void AccountBox_SelectedIndexChanged(object sender, EventArgs e) { CharacterBox.DataSource = ((Server.Account)AccountBox.SelectedItem).Characters; }
+
+        private void CharacterBox_SelectedIndexChanged(object sender, EventArgs e) {
+            StylesBox.Items.Clear();
+            foreach(var style in ((Server.Account.Character)CharacterBox.SelectedItem).Styles)
+                StylesBox.Items.Add(style.Key);
+        }
+
+        private class Server {
+            internal readonly List<Account> Accounts = new List<Account>();
+            private readonly string _name;
+
+            public Server(string dir) {
+                _name = Path.GetFileName(dir);
+                var tmp = Path.Combine(Path.Combine(dir, "SavedVariables"), "CraftExport.lua");
+                if(File.Exists(tmp))
+                    ParseTable(tmp);
+            }
+
+            private static bool IsRubbish(string line) {
+                return line.Trim().Equals("{") || line.Trim().Equals("}") || (line.Trim().StartsWith("CraftExport =") || line.Trim().Equals("[\"version\"] = 1,", StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            private static bool IsAccount(string line) { return line.Trim().StartsWith("[\"@") && line.Trim().EndsWith("\"] ="); }
+
+            private static string GetSectionName(string line) {
+                line = line.Trim();
+                line = line.Substring(2);
+                return line.Substring(0, line.Length - 4);
+            }
+
+            private void ParseTable(string file) {
+                Account acc = null;
+                Account.Character Char = null;
+                var endMarkerReached = false;
+                var sectionEndMarkerReached = false;
+                var currentSection = "";
+                //var subSection = "";
+                foreach(var line in File.ReadAllLines(file)) {
+                    if(IsRubbish(line))
+                        continue;
+                    if(IsAccount(line)) {
+                        if(acc != null)
+                            Accounts.Add(acc);
+                        acc = new Account(GetSectionName(line));
+                        continue;
+                    }
+                    if(acc == null)
+                        continue;
+                    if(IsEndMarker(line)) {
+                        if (sectionEndMarkerReached)
+                            endMarkerReached = true;
+                        else if(!endMarkerReached)
+                            sectionEndMarkerReached = true;
+                        if(Char != null && Char.IsComplete) {
+                            acc.Characters.Add(Char);
+                            Char = null;
+                            endMarkerReached = true;
+                            sectionEndMarkerReached = false;
+                        }
+                        if (sectionEndMarkerReached)
+                            currentSection = "";
+                        continue;
+                    }
+                    if(Char == null) {
+                        Char = new Account.Character(GetSectionName(line));
+                        continue;
+                    }
+                    if(string.IsNullOrEmpty(currentSection)) {
+                        currentSection = GetSectionName(line);
+                        continue;
+                    }
+                    //if(string.IsNullOrEmpty(subSection))
+                        Char.AddSection(currentSection, line);
+                    //else
+                        //Char.AddSection(currentSection, subSection, line);
+                }
+                if(acc != null) {
+                    if(Char != null)
+                        acc.Characters.Add(Char);
+                    Accounts.Add(acc);
+                }
+            }
+
+            private bool IsEndMarker(string line) { return line.Trim().Equals("},"); }
+
+            public override string ToString() { return _name; }
+
+            internal class Account {
+                private readonly string _name;
+                internal readonly List<Character> Characters = new List<Character>();
+
+                public Account(string name) { _name = name; }
+
+                public override string ToString() { return _name; }
+
+                internal class Character {
+                    internal readonly Dictionary<string, bool> Styles = new Dictionary<string, bool>();
+
+                    private readonly string _name;
+
+                    public Character(string name) { _name = name; }
+
+                    public bool IsComplete { get { return Styles.Count == 14; } }
+
+                    public override string ToString() { return _name; }
+
+                    public void AddSection(string currentSection, string line) {
+                        if(currentSection.Equals("Styles", StringComparison.CurrentCultureIgnoreCase)) {
+                            var style = line.Trim().Substring(2);
+                            style = style.Substring(0, style.Length - 7);
+                            if (!Styles.ContainsKey(style))
+                                Styles.Add(style, line.EndsWith("1,"));
+                        }
+                    }
+
+                    public void AddSection(string currentSection, string subSection, string line) {
+                        if(currentSection.Equals("Woodworking", StringComparison.CurrentCultureIgnoreCase)) {}
+                        else if(currentSection.Equals("Blacksmithing", StringComparison.CurrentCultureIgnoreCase)) {}
+                        else if(currentSection.Equals("Clothing_Light", StringComparison.CurrentCultureIgnoreCase)) {}
+                        else if(currentSection.Equals("Clothing_Medium", StringComparison.CurrentCultureIgnoreCase)) {}
+                    }
+                }
+            }
+        }
+    }
+}
